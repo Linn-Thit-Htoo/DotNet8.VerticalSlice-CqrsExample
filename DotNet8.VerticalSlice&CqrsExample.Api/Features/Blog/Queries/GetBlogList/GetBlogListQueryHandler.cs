b4dfe57974e9dc6efd6037815fb02fcc022fcec9
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-04 18:03:11 +0630

    -

--- DotNet8.VerticalSlice&CqrsExample.Api/Features/Blog/Queries/GetBlogList/GetBlogListQueryHandler.cs
+++ DotNet8.VerticalSlice&CqrsExample.Api/Features/Blog/Queries/GetBlogList/GetBlogListQueryHandler.cs
@@ -2,20 +2,19 @@
 using DotNet8.VerticalSlice_CqrsExample.Models.Setup.Blog;
 using MediatR;
 
-namespace DotNet8.VerticalSlice_CqrsExample.Api.Features.Blog.Queries.GetBlogList
+namespace DotNet8.VerticalSlice_CqrsExample.Api.Features.Blog.Queries.GetBlogList;
+
+public class GetBlogListQueryHandler : IRequestHandler<GetBlogListQuery, BlogListResponseModel>
 {
-    public class GetBlogListQueryHandler : IRequestHandler<GetBlogListQuery, BlogListResponseModel>
+    private readonly IBlogRepository _blogRepository;
+
+    public GetBlogListQueryHandler(IBlogRepository blogRepository)
     {
-        private readonly IBlogRepository _blogRepository;
+        _blogRepository = blogRepository;
+    }
 
-        public GetBlogListQueryHandler(IBlogRepository blogRepository)
-        {
-            _blogRepository = blogRepository;
-        }
-
-        public async Task<BlogListResponseModel> Handle(GetBlogListQuery request, CancellationToken cancellationToken)
-        {
-            return await _blogRepository.GetBlogsAsync();
-        }
+    public async Task<BlogListResponseModel> Handle(GetBlogListQuery request, CancellationToken cancellationToken)
+    {
+        return await _blogRepository.GetBlogsAsync();
     }
 }
