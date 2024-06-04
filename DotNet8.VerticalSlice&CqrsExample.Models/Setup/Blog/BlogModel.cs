ed1aea2b89c5c6bac16b2908e871421856cf3da7
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-04 18:13:00 +0630

    -

--- DotNet8.VerticalSlice&CqrsExample.Models/Setup/Blog/BlogModel.cs
+++ DotNet8.VerticalSlice&CqrsExample.Models/Setup/Blog/BlogModel.cs
@@ -1,10 +1,9 @@
-﻿namespace DotNet8.VerticalSlice_CqrsExample.Models.Setup.Blog
+﻿namespace DotNet8.VerticalSlice_CqrsExample.Models.Setup.Blog;
+
+public class BlogModel
 {
-    public class BlogModel
-    {
-        public long BlogId { get; set; }
-        public string BlogTitle { get; set; }
-        public string BlogAuthor { get; set; }
-        public string BlogContent { get; set; }
-    }
+    public long BlogId { get; set; }
+    public string BlogTitle { get; set; }
+    public string BlogAuthor { get; set; }
+    public string BlogContent { get; set; }
 }
