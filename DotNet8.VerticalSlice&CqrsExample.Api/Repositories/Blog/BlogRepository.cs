72819763580c03c319dbf4fb7df26c38135d229e
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-04 18:17:21 +0630

    -

--- DotNet8.VerticalSlice&CqrsExample.Api/Repositories/Blog/BlogRepository.cs
+++ DotNet8.VerticalSlice&CqrsExample.Api/Repositories/Blog/BlogRepository.cs
@@ -81,6 +81,9 @@ public class BlogRepository : IBlogRepository
 
     #endregion
 
+    #region MyRegion
+
+    #endregion
     public async Task<BlogModel> GetBlogByIdAsync(long id)
     {
         try
