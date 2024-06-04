ee56c04839dc62aef637fb18a449bad0fd75651f
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-04 18:06:49 +0630

    -

--- DotNet8.VerticalSlice&CqrsExample.Api/ModularService.cs
+++ DotNet8.VerticalSlice&CqrsExample.Api/ModularService.cs
@@ -43,7 +43,7 @@ namespace DotNet8.VerticalSlice_CqrsExample.Api
 
         #endregion
 
-        #region MyRegion
+        #region AddMediatRService
 
         #endregion
         private static IServiceCollection AddMediatRService(this IServiceCollection services)
