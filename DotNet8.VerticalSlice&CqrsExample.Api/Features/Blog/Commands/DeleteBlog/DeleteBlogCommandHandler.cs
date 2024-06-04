5de7fbc88d97cafd5098ee9ca5257e807f78f598
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-04 17:46:28 +0630

    -

--- DotNet8.VerticalSlice&CqrsExample.Api/Features/Blog/Commands/DeleteBlog/DeleteBlogCommandHandler.cs
+++ DotNet8.VerticalSlice&CqrsExample.Api/Features/Blog/Commands/DeleteBlog/DeleteBlogCommandHandler.cs
@@ -1,23 +1,22 @@
 ﻿using DotNet8.VerticalSlice_CqrsExample.Api.Repositories.Blog;
 using MediatR;
 
-namespace DotNet8.VerticalSlice_CqrsExample.Api.Features.Blog.Commands.DeleteBlog
+namespace DotNet8.VerticalSlice_CqrsExample.Api.Features.Blog.Commands.DeleteBlog;
+
+public class DeleteBlogCommandHandler : IRequestHandler<DeleteBlogCommand, int>
 {
-    public class DeleteBlogCommandHandler : IRequestHandler<DeleteBlogCommand, int>
+    private readonly IBlogRepository _blogRepository;
+
+    public DeleteBlogCommandHandler(IBlogRepository blogRepository)
     {
-        private readonly IBlogRepository _blogRepository;
+        _blogRepository = blogRepository;
+    }
 
-        public DeleteBlogCommandHandler(IBlogRepository blogRepository)
-        {
-            _blogRepository = blogRepository;
-        }
+    public async Task<int> Handle(DeleteBlogCommand request, CancellationToken cancellationToken)
+    {
+        if (request.BlogId <= 0)
+            throw new Exception("Id is invalid,");
 
-        public async Task<int> Handle(DeleteBlogCommand request, CancellationToken cancellationToken)
-        {
-            if (request.BlogId <= 0)
-                throw new Exception("Id is invalid,");
-
-            return await _blogRepository.DeleteBlogAsync(request.BlogId);
-        }
+        return await _blogRepository.DeleteBlogAsync(request.BlogId);
     }
 }
