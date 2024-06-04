541c6a7868bbcae9f1b804499f3780819fcab3df
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-04 17:45:55 +0630

    -

--- DotNet8.VerticalSlice&CqrsExample.Api/Features/Blog/Commands/CreateBlog/CreateBlogCommandHandler.cs
+++ DotNet8.VerticalSlice&CqrsExample.Api/Features/Blog/Commands/CreateBlog/CreateBlogCommandHandler.cs
@@ -2,29 +2,28 @@
 using DotNet8.VerticalSlice_CqrsExample.Models.Setup.Blog;
 using MediatR;
 
-namespace DotNet8.VerticalSlice_CqrsExample.Api.Features.Blog.Commands.CreateBlog
+namespace DotNet8.VerticalSlice_CqrsExample.Api.Features.Blog.Commands.CreateBlog;
+
+public class CreateBlogCommandHandler : IRequestHandler<CreateBlogCommand, int>
 {
-    public class CreateBlogCommandHandler : IRequestHandler<CreateBlogCommand, int>
+    private readonly IBlogRepository _blogRepository;
+
+    public CreateBlogCommandHandler(IBlogRepository blogRepository)
     {
-        private readonly IBlogRepository _blogRepository;
+        _blogRepository = blogRepository;
+    }
 
-        public CreateBlogCommandHandler(IBlogRepository blogRepository)
-        {
-            _blogRepository = blogRepository;
-        }
+    public async Task<int> Handle(CreateBlogCommand request, CancellationToken cancellationToken)
+    {
+        if (string.IsNullOrEmpty(request.BlogRequestModel.BlogTitle))
+            throw new Exception("Blog Title cannot be empty.");
 
-        public async Task<int> Handle(CreateBlogCommand request, CancellationToken cancellationToken)
-        {
-            if (string.IsNullOrEmpty(request.BlogRequestModel.BlogTitle))
-                throw new Exception("Blog Title cannot be empty.");
+        if (string.IsNullOrEmpty(request.BlogRequestModel.BlogAuthor))
+            throw new Exception("Blog Author cannot be empty.");
 
-            if (string.IsNullOrEmpty(request.BlogRequestModel.BlogAuthor))
-                throw new Exception("Blog Author cannot be empty.");
+        if (string.IsNullOrEmpty(request.BlogRequestModel.BlogContent))
+            throw new Exception("Blog Content cannot be empty.");
 
-            if (string.IsNullOrEmpty(request.BlogRequestModel.BlogContent))
-                throw new Exception("Blog Content cannot be empty.");
-
-            return await _blogRepository.CreateBlogAsync(request.BlogRequestModel);
-        }
+        return await _blogRepository.CreateBlogAsync(request.BlogRequestModel);
     }
 }
