a9a444af54e98a1c3371e0277ee52fc109eae357
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-04 18:11:09 +0630

    -

--- DotNet8.VerticalSlice&CqrsExample.Models/ChangeModel.cs
+++ DotNet8.VerticalSlice&CqrsExample.Models/ChangeModel.cs
@@ -1,29 +1,28 @@
 ﻿using DotNet8.VerticalSlice_CqrsExample.DbService.Entities;
 using DotNet8.VerticalSlice_CqrsExample.Models.Setup.Blog;
 
-namespace DotNet8.VerticalSlice_CqrsExample.Models
-{
-    public static class ChangeModel
-    {
-        public static BlogModel Change(this TblBlog dataModel)
-        {
-            return new BlogModel
-            {
-                BlogId = dataModel.BlogId,
-                BlogTitle = dataModel.BlogTitle,
-                BlogAuthor = dataModel.BlogAuthor,
-                BlogContent = dataModel.BlogContent
-            };
-        }
+namespace DotNet8.VerticalSlice_CqrsExample.Models;
 
-        public static TblBlog Change(this BlogRequestModel requestModel)
+public static class ChangeModel
+{
+    public static BlogModel Change(this TblBlog dataModel)
+    {
+        return new BlogModel
         {
-            return new TblBlog
-            {
-                BlogTitle = requestModel.BlogTitle!,
-                BlogAuthor = requestModel.BlogAuthor!,
-                BlogContent = requestModel.BlogContent!
-            };
-        }
+            BlogId = dataModel.BlogId,
+            BlogTitle = dataModel.BlogTitle,
+            BlogAuthor = dataModel.BlogAuthor,
+            BlogContent = dataModel.BlogContent
+        };
+    }
+
+    public static TblBlog Change(this BlogRequestModel requestModel)
+    {
+        return new TblBlog
+        {
+            BlogTitle = requestModel.BlogTitle!,
+            BlogAuthor = requestModel.BlogAuthor!,
+            BlogContent = requestModel.BlogContent!
+        };
     }
 }
