90140b8360bffc8d1652462b18248c679d6d85a2
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-04 18:04:33 +0630

    -

--- DotNet8.VerticalSlice&CqrsExample.Api/Repositories/Blog/IBlogRepository.cs
+++ DotNet8.VerticalSlice&CqrsExample.Api/Repositories/Blog/IBlogRepository.cs
@@ -1,13 +1,12 @@
 ﻿using DotNet8.VerticalSlice_CqrsExample.Models.Setup.Blog;
 
-namespace DotNet8.VerticalSlice_CqrsExample.Api.Repositories.Blog
+namespace DotNet8.VerticalSlice_CqrsExample.Api.Repositories.Blog;
+
+public interface IBlogRepository
 {
-    public interface IBlogRepository
-    {
-        Task<BlogListResponseModel> GetBlogsAsync();
-        Task<BlogModel> GetBlogByIdAsync(long id);
-        Task<int> CreateBlogAsync(BlogRequestModel requestModel);
-        Task<int> UpdateBlogAsync(BlogRequestModel requestModel, long id);
-        Task<int> DeleteBlogAsync(long id);
-    }
+    Task<BlogListResponseModel> GetBlogsAsync();
+    Task<BlogModel> GetBlogByIdAsync(long id);
+    Task<int> CreateBlogAsync(BlogRequestModel requestModel);
+    Task<int> UpdateBlogAsync(BlogRequestModel requestModel, long id);
+    Task<int> DeleteBlogAsync(long id);
 }
